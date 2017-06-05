a923d3d68cd16c5ebdf5f8edf7d97602d616ca3b
AAKASH HANDA <AAKASH HANDA>
2017-06-05 19:02:39 +0530

    A

--- app/app.component.js
+++ app/app.component.js
@@ -10,14 +10,14 @@ var core_1 = require("@angular/core");
 var product_service_1 = require("./products/product.service");
 var AppComponent = (function () {
     function AppComponent() {
-        this.pageTitle = "Angular 2 App";
+        this.pageTitle = "Angular2";
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: "\n    <div><h1>{{pageTitle}}</h1>\n        <pm-products></pm-products>\n    </div>\n    ",
+        template: "\n    <div>\n        <nav class='navbar navbar-default'>\n            <div class='container-fluid'>\n                <a class='navbar-brand'>{{pageTitle}}</a>\n                <ul class='nav navbar-nav'>\n                    <li><a [routerLink]=\"['/welcome']\">Home</a></li>\n                    <li><a [routerLink]=\"['/products']\">Product List</a></li>\n                </ul>\n            </div>\n        </nav>\n        <div class='container'>\n            <router-outlet></router-outlet>\n        </div>\n     </div>\n     ",
         providers: [product_service_1.ProductService]
     })
 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import {ProductService} from './products/product.service';
+
+import { ProductService } from './products/product.service';
 
 @Component({
     selector: 'pm-app',
     template: `
-    <div><h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
-    </div>
-    `,
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `,
     providers: [ ProductService ]
 })
 export class AppComponent {
-    pageTitle: string = `Angular 2 App`;
+    pageTitle: string = `Angular2`;
 }
