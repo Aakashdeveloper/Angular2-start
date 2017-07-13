392007281d26005b39d59d3c532b99077380d869
AAKASH HANDA <AAKASH HANDA>
2017-07-13 19:36:35 +0530

    A

--- app/products/product.service.js
+++ app/products/product.service.js
@@ -11,7 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
-var Observable_1 = require("rxjs/Observable");
+var observable_1 = require("rxjs/observable");
 require("rxjs/add/operator/catch");
 require("rxjs/add/operator/do");
 require("rxjs/add/operator/map");
@@ -28,7 +28,7 @@ var ProductService = (function () {
     };
     ProductService.prototype.handleError = function (error) {
         console.error(error);
-        return Observable_1.Observable.throw(error.json().error || 'Server error');
+        return observable_1.Observable.throw(error.json().error || 'Server error');
     };
     return ProductService;
 }());
--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
